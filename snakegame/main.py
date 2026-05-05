0c94e3d9df214be3f740b092781e2be5aa7d11ed
acloudnerd <tshepo.matshisevhe@icloud.com>
2026-05-05 13:58:19 +0200

    Initial version of the snake game, can update scores after eating food and can move in different directions

---
+++ snakegame/main.py
@@ -0,0 +1,36 @@
+from turtle import Screen
+from snake import Snake
+import time
+from food import Food
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Welcome to Snake Park!")
+screen.tracer(0)
+
+snake = Snake()
+snake.create_snake()
+
+food = Food()
+
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_on = True
+while game_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_the_score()
+
+screen.exitonclick()
