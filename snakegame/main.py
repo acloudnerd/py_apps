3c31e44fb0fe4bbf343baa5ef442fba275e76633
acloudnerd <tshepo.matshisevhe@icloud.com>
2026-05-05 15:27:55 +0200

    introduction to slicing

--- snakegame/main.py
+++ snakegame/main.py
@@ -39,10 +39,8 @@ while game_on:
         scoreboard.game_over()
 
     # detect collision with the tail
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
             game_on = False
             scoreboard.game_over()
 
