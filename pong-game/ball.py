e16f99d4f780bda27f36b02130812b6ab13dea4c
acloudnerd <tshepo.matshisevhe@icloud.com>
2026-05-12 09:43:29 +0200

    The pong game

--- pong-game/ball.py
+++ pong-game/ball.py
@@ -1,7 +1,7 @@
 from turtle import Turtle
-import random
 
-ORIGIN = (0,0)
+ORIGIN = (0, 0)
+
 
 class Ball(Turtle):
     def __init__(self):
@@ -10,13 +10,23 @@ class Ball(Turtle):
         self.goto(ORIGIN)
         self.color("yellow")
         self.penup()
-        self.move()
+        self.x_move = 10
+        self.y_move = 10
+        self.move_speed = 0.1
 
     def move(self):
-        # random_x = random.randint(-280, 280)
-        # random_y = random.randint(-280, 280)
-        # self.goto(random_x, random_y)
-        new_x = self.xcor() + 10
-        new_y = self.ycor() + 10
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
 
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
+        self.move_speed *= 0.9
+
+    def reset_position(self):
+        self.goto(0, 0)
+        self.move_speed = 0.1
+        self.bounce_x()
--- pong-game/main.py
+++ pong-game/main.py
@@ -2,6 +2,7 @@ import time
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.bgcolor("Black")
@@ -15,6 +16,9 @@ l_paddle.color("pink")
 
 ball = Ball()
 
+scoreboard = Scoreboard()
+scoreboard.update_score()
+
 screen.listen()
 screen.onkey(r_paddle.move_up, "Up")
 screen.onkey(r_paddle.move_down, "Down")
@@ -25,7 +29,23 @@ game_on = True
 
 while game_on:
     screen.update()
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     ball.move()
+
+    # Detect collision with the wall above and below
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect collision between the ball and the paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 260 or ball.distance(l_paddle) < 50 and ball.xcor() < -260:
+        ball.bounce_x()
+
+    if ball.xcor() > 300:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    if ball.xcor() < -300:
+        ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
