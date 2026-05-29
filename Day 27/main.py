700bc8ec2913055481a7dc390bc1d337631a0706
acloudnerd <tshepo.matshisevhe@icloud.com>
2026-05-29 13:04:34 +0200

    Understanding *args and ***kwargs

--- Day 27/main.py
+++ Day 27/main.py
@@ -1,16 +1,30 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
 
 # labelling
 
-my_label = tkinter.Label(text="I am a label", font=("Arial", 34, "italic"))
+entry = Entry(width=10)
+entry.pack(side="top")
 
-my_label.pack(expand=1)
 
+def button_clicked():
+    output = entry.get()
+    my_label = Label(text=output, font=("Arial", 34, "italic"))
+    my_label.pack(expand=1)
+
+
+button = Button(text="Click Me", command = button_clicked)
+button.pack(side="top")
 
 
 window.mainloop()
+
+# def add(*args):
+#     print(sum(args))
+
+
+# add(1,2,3,4,5,6,7,8,9,10)
