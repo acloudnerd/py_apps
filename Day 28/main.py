87e8e8af1163151959750bf769f552f1ed51afce
acloudnerd <tshepo.matshisevhe@icloud.com>
2026-06-09 19:25:24 +0200

    Added a count down mechanism

--- Day 28/main.py
+++ Day 28/main.py
@@ -1,5 +1,7 @@
+import math
 from tkinter import *
 from PIL import Image, ImageTk
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -11,17 +13,34 @@ SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 FONT_TUPLE = (FONT_NAME, 50, "bold")
 
-# ---------------------------- TIMER RESET ------------------------------- # 
 
+# ---------------------------- TIMER RESET ------------------------------- #
+def reset_timer():
+    count_down(0)
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    count_down(5)
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        print(count)
+        window.after(1000, count_down, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Lufuno-ro")
 window.config(padx=100, pady=10, bg=PINK)
 window.resizable(False, False)
+
+###############
+
+############
+
 
 title_label = Label(text="Timer", fg=RED, font=(FONT_NAME, 50, "bold"), bg=PINK)
 title_label.grid(column=1, row=0)
@@ -34,16 +53,16 @@ background = Image.new("RGBA", (CANVAS_W, CANVAS_H), PINK)
 background.paste(pil_img, mask=pil_img.split()[3])
 lufuno_img = ImageTk.PhotoImage(background.convert("RGB"))
 canvas.create_image(CANVAS_W // 2, CANVAS_H // 2, image=lufuno_img)
-canvas.create_text(CANVAS_W // 2, CANVAS_H // 2 + 50, text="00:00", font=FONT_TUPLE, fill=RED)
+timer_text = canvas.create_text(CANVAS_W // 2, CANVAS_H // 2 + 50, text="00:00", font=FONT_TUPLE, fill=RED)
 canvas.grid(column=1, row=1)
 
-start_button = Button(text="Start", highlightthickness=0, )
+
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=1)
-reset_button = Button(text="Reset", highlightthickness=0)
+reset_button = Button(text="Reset", highlightthickness=0, command=reset_timer)
 reset_button.grid(column=2, row=1)
 
 check_marks = Label(text="✔️", fg=GREEN, bg=PINK, highlightthickness=0)
 check_marks.grid(column=1, row=2)
-
 
 window.mainloop()
