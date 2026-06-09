2f13a2006550909d1d9dede66f201151e186f1af
acloudnerd <tshepo.matshisevhe@icloud.com>
2026-06-09 20:34:53 +0200

    Understood dynamic typing and added functionality to set different timer sessions

--- Day 28/main.py
+++ Day 28/main.py
@@ -8,28 +8,46 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
+WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 FONT_TUPLE = (FONT_NAME, 50, "bold")
-
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- #
 def reset_timer():
     count_down(0)
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    count_down(5)
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        title_label.config(text="Long Break", fg=GREEN, font=(FONT_NAME, 50, "bold"), bg=PINK)
+        count_down(long_break_sec)
+    elif reps % 2 == 0:
+        title_label.config(text="Short Break", fg=YELLOW, font=(FONT_NAME, 50, "bold"), bg=PINK)
+        count_down(short_break_sec)
+    else:
+        title_label.config(text="Busy Working", fg=RED, font=(FONT_NAME, 50, "bold"), bg=PINK)
+        count_down(work_sec)
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
 def count_down(count):
     count_min = math.floor(count / 60)
     count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         print(count)
         window.after(1000, count_down, count - 1)
-
+    else:
+        start_timer()
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
