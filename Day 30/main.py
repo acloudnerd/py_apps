1bb3175cbd06dab1fcd2ae9038538011ccb330f7
acloudnerd <tshepo.matshisevhe@icloud.com>
2026-06-29 18:48:54 +0200

    created a search functionality for password manager and also handled exceptions

--- Day 30/main.py
+++ Day 30/main.py
@@ -1,15 +1,25 @@
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
-import pandas
+import csv
 
-data = pandas.read_csv("nato_phonetic_alphabet.csv")
-#TODO 1. Create a dictionary in this format:
-phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+with open("nato_phonetic_alphabet.csv", newline="") as csv_file:
+    data = csv.DictReader(csv_file)
+    #TODO 1. Create a dictionary in this format:
+    phonetic_dict = {row["letter"]: row["code"] for row in data}
+
 print(phonetic_dict)
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters allowed.")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+generate_phonetic()
