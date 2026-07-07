b711dab3daafa5da048bb1a6439644306f427342
acloudnerd <tshepo.matshisevhe@icloud.com>
2026-07-07 10:53:48 +0200

    created the kanye quotes app

--- Day 33/main.py
+++ Day 33/main.py
@@ -1,4 +1,16 @@
 import requests
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
-print(response.status_code)
+# print(response.status_code)
+response.raise_for_status()
+
+data = response.json()
+
+# print(data["iss_position"]["latitude"])
+
+latitude = data["iss_position"]["latitude"]
+longitude = data["iss_position"]["longitude"]
+
+the_current_position = (latitude, longitude)
+
+print(the_current_position)
