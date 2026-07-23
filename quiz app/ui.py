a35a49125807512ae8e4ce1e9fe6793947dc1243
acloudnerd <tshepo.matshisevhe@icloud.com>
2026-07-23 09:29:45 +0200

    button functionality

--- quiz app/ui.py
+++ quiz app/ui.py
@@ -32,11 +32,11 @@ class QuizInterface:
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
         right_image = PhotoImage(file="images/true.png")
-        self.right = Button(image=right_image, highlightthickness=0)
+        self.right = Button(image=right_image, highlightthickness=0, command=self.true_pressed)
         self.right.grid(row=2, column=0)
 
         wrong_image = PhotoImage(file="images/false.png")
-        self.wrong = Button(image=wrong_image, highlightthickness=0)
+        self.wrong = Button(image=wrong_image, highlightthickness=0, command=self.false_pressed)
         self.wrong.grid(row=2, column=1)
 
         self.get_next_question()
@@ -46,3 +46,11 @@ class QuizInterface:
     def get_next_question(self):
         q_text = self.quiz.next_question()
         self.canvas.itemconfig(self.question_text, text = q_text)
+
+    def true_pressed(self):
+        self.give_feedback(self.quiz.check_answer("True"))
+
+    def false_pressed(self):
+        is_right = self.quiz.check_answer("False")
+
+    def give_feedback(self, is_right):
